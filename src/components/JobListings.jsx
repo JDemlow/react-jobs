0225e462a8c235637da485b81a9f25d2ad066582
JDemlow <jdemlowdev@gmail.com>
2024-04-26 15:55:03 -0600

    adding dynamic values

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,17 +1,17 @@
 import JobListing from "./JobListing";
 import jobs from "../jobs.json";
 
-const JobListings = () => {
-  const recentJobs = jobs.slice(0, 3);
+const JobListings = ({ isHome = false }) => {
+  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
 
   return (
     <section className="px-4 py-10 bg-blue-50">
       <div className="m-auto container-xl lg:container">
         <h2 className="mb-6 text-3xl font-bold text-center text-indigo-500">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
-          {recentJobs.map((job) => (
+          {jobListings.map((job) => (
             <JobListing key={job.id} job={job} />
           ))}
         </div>
--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,5 +1,10 @@
+import JobListings from "../components/JobListings";
 const JobsPage = () => {
-  return <div>JobsPage</div>;
+  return (
+    <div className="px-4 py-6 bg-blue-50">
+      <JobListings />
+    </div>
+  );
 };
 
 export default JobsPage;
